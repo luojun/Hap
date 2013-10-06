bd49a389b272585f1ad4d3df6c19bca9862c927a
Jun Luo <jun.luo@ageofsignificance.org>
2013-10-06 12:33:13 -0400

    initial TTS facility

--- AndroidStudioProject/MyApplication/src/main/java/ca/dragonflystudios/hap/HapActivity.java
+++ AndroidStudioProject/MyApplication/src/main/java/ca/dragonflystudios/hap/HapActivity.java
@@ -7,20 +7,26 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-public class HapActivity extends Activity {
+import java.util.Locale;
+
+public class HapActivity extends Activity implements TextToSpeech.OnInitListener {
     private static final boolean LOG = false;
     private AcceGyroGraphView mGraphView;
     private AcceGyro.Logger mLogger;
     private AcceGyro mAcceGyro;
     private GyroGestureRecognizer mRecognizer;
 
+    private TextToSpeech mTts;
+
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.w(getClass().getName(), "received: " + intent.getStringExtra("TYPE") + " " + intent.getStringExtra("VALUE"));
+            mTts.speak(intent.getStringExtra("VALUE"), TextToSpeech.QUEUE_FLUSH, null);
         }
     };
 
@@ -28,6 +34,7 @@ public class HapActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mTts = new TextToSpeech(this, this);
         mGraphView = new AcceGyroGraphView(this);
         mAcceGyro = new AcceGyro((SensorManager) getSystemService(SENSOR_SERVICE));
         mRecognizer = new GyroGestureRecognizer(this);
@@ -57,6 +64,22 @@ public class HapActivity extends Activity {
     @Override
     protected void onDestroy() {
         mAcceGyro.deleteObservers();
+        if (mTts != null)
+            mTts.shutdown();
         super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            Log.w(getClass().getName(), "TTS initialized.");
+            int result = mTts.setLanguage(Locale.CANADA);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.w(getClass().getName(), "Locale Canada is not supported");
+            }
+        } else
+            Log.w(getClass().getName(), "Failed to initialize TTS.");
     }
 }
