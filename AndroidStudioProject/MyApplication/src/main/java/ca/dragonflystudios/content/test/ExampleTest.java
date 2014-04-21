48ad6b4c8ab933936eaf17318179aec67f69c2e0
luojun <jun@dragonflystudios.ca>
2014-04-21 00:39:57 -0400

    start using unit test: a sample dummy  test.

---
+++ AndroidStudioProject/MyApplication/src/main/java/ca/dragonflystudios/content/test/ExampleTest.java
@@ -0,0 +1,15 @@
+package ca.dragonflystudios.content.test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by jun on 2014-04-21.
+ */
+public class ExampleTest extends InstrumentationTestCase
+{
+    public void test() throws Exception {
+        final int expected = 0;
+        final int reality = 1;
+        assertEquals(expected, reality);
+    }
+}
